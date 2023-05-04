19f874e169ab969edced6f07595ac09ea29c8fc8
Jorge Balderas <jorgerbf@gmail.com>
2023-05-04 09:24:28 -0400

    Unit tests update
    Updated unit tests as they are not currently passing because the other methods are not implemented

--- functions-quarkus/src/test/java/io/quarkus/GreetingTest.java
+++ functions-quarkus/src/test/java/io/quarkus/GreetingTest.java
@@ -12,30 +12,6 @@ public class GreetingTest
     // NOTE: RestAssured is aware of the application.properties quarkus.http.root-path switch
 
     @Test
-    public void testJaxrs() {
-        RestAssured.when().get("/hello").then()
-                .statusCode(200)
-                .contentType("text/plain")
-                .body(equalTo("hello jaxrs"));
-    }
-
-    @Test
-    public void testServlet() {
-        RestAssured.when().get("/servlet/hello").then()
-                .statusCode(200)
-                .contentType("text/plain")
-                .body(equalTo("hello servlet"));
-    }
-
-    @Test
-    public void testVertx() {
-        RestAssured.when().get("/vertx/hello").then()
-                .statusCode(200)
-                .contentType("text/plain")
-                .body(equalTo("hello vertx"));
-    }
-
-    @Test
     public void testFunqy() {
         RestAssured.when().get("/funqyHello").then()
                 .statusCode(200)
