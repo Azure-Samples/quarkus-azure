973813a27d94362d6714fcc5c1265c863455b623
Jianguo Ma <jiangma@microsoft.com>
2024-11-21 14:00:17 +0800

    apply same changes to todo frontend for sample aks-quarkus as well
    Signed-off-by: Jianguo Ma <jiangma@microsoft.com>

--- aks-quarkus/src/main/resources/META-INF/resources/js/routes.js
+++ aks-quarkus/src/main/resources/META-INF/resources/js/routes.js
@@ -8,14 +8,16 @@
 
     ['all', 'active', 'completed'].forEach(function (visibility) {
         router.on(visibility, function () {
-            app.visibility = visibility;
+            // a trick way to update property defined in Vue.js 3 component
+            // reference: https://stackoverflow.com/questions/68632488/how-do-i-access-vue-instance-inside-a-js-file-in-vue3 & https://jsfiddle.net/9hc6rza2/
+            app._instance.ctx.$data.visibility = visibility;
         });
     });
 
     router.configure({
         notfound: function () {
             window.location.hash = '';
-            app.visibility = 'all';
+            app._instance.ctx.$data.visibility = 'all';
         }
     });
 
