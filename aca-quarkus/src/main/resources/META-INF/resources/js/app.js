69134ec71ce43f30721f45ae086976b19473882c
Jianguo Ma <jiangma@microsoft.com>
2024-11-21 10:52:29 +0800

    upgrade vuejs from 2 to 3 using the global build
    Signed-off-by: Jianguo Ma <jiangma@microsoft.com>

--- aca-quarkus/src/main/resources/META-INF/resources/js/app.js
+++ aca-quarkus/src/main/resources/META-INF/resources/js/app.js
@@ -1,136 +1,134 @@
 /*global Vue, todoStorage */
+var filters = {
+    all: function (todos) {
+        return todos;
+    },
+    active: function (todos) {
+        return todos.filter(function (todo) {
+            return !todo.completed;
+        });
+    },
+    completed: function (todos) {
+        return todos.filter(function (todo) {
+            return todo.completed;
+        });
+    }
+};
 
-(function (exports) {
-
-    'use strict';
-
-    var filters = {
-        all: function (todos) {
-            return todos;
-        },
-        active: function (todos) {
-            return todos.filter(function (todo) {
-                return !todo.completed;
-            });
-        },
-        completed: function (todos) {
-            return todos.filter(function (todo) {
-                return todo.completed;
-            });
+const todoFocus = {
+    mounted(el, binding) {
+        if (binding.value) {
+            el.focus();
         }
-    };
+    }
+};
 
-    exports.app = new Vue({
+const { createApp } = Vue
 
-        // the root element that will be compiled
-        el: '.todoapp',
-
-        // app initial state
-        data: {
+var app = createApp({
+    // app initial state
+    data() {
+        return {
             todos: [],
             newTodo: '',
             editedTodo: null,
             visibility: 'all'
-        },
-
-        computed: {
-            filteredTodos: function () {
-                return filters[this.visibility](this.todos);
-            },
-            remaining: function () {
-                return filters.active(this.todos).length;
-            },
-            allDone: {
-                get: function () {
-                    return this.remaining === 0;
-                },
-                set: function (value) {
-                    this.todos.forEach(function (todo) {
-                        todo.completed = value;
-                    });
-                }
-            }
-        },
-
-        methods: {
-
-            pluralize: function (word, count) {
-                return word + (count === 1 ? '' : 's');
-            },
-
-            addTodo: async function () {
-                var value = this.newTodo && this.newTodo.trim();
-                if (!value) {
-                    return;
-                }
-
-                const item = await todoStorage.add({
-                    title : value,
-                    order: this.todos.length + 1,
-                    completed: false
-                });
-                this.todos.push(item);
-                this.newTodo = '';
-            },
-
-            removeTodo: async function (todo) {
-                await todoStorage.delete(todo);
-                await this.reload();
-            },
-
-            toggleTodo: function (todo) {
-                todo.completed = ! todo.completed;
-                todoStorage.save(todo);
-            },
-
-            editTodo: async function (todo) {
-                this.beforeEditCache = todo.title;
-                this.editedTodo = todo;
-            },
-
-            doneEdit: function (todo) {
-                if (!this.editedTodo) {
-                    return;
-                }
-                this.editedTodo = null;
-                todo.title = todo.title.trim();
-                if (!todo.title) {
-                    this.removeTodo(todo);
-                } else {
-                    todoStorage.save(todo);
-                }
-            },
-
-            cancelEdit: function (todo) {
-                this.editedTodo = null;
-                todo.title = this.beforeEditCache;
-            },
-
-            removeCompleted: async function () {
-                await todoStorage.deleteCompleted();
-                await this.reload();
-            },
-
-            reload: async function () {
-                const data = await todoStorage.fetch();
-                app.todos = data;
-            }
-        },
-
-        // a custom directive to wait for the DOM to be updated
-        // before focusing on the input field.
-        // http://vuejs.org/guide/custom-directive.html
-        directives: {
-            'todo-focus': function (el, binding) {
-                if (binding.value) {
-                    el.focus();
-                }
-            }
-        },
-
-        mounted : async function() {
-            this.reload();
         }
-    });
+    },
 
-})(window);
+    computed: {
+        filteredTodos() {
+            return filters[this.visibility](this.todos);
+        },
+        remaining() {
+            return filters.active(this.todos).length;
+        },
+        allDone: {
+            get() {
+                return this.remaining === 0;
+            },
+            set(value) {
+                this.todos.forEach(function (todo) {
+                    todo.completed = value;
+                    todoStorage.save(todo);
+                });
+            }
+        }
+    },
+
+    methods: {
+        pluralize(word, count) {
+            return word + (count === 1 ? '' : 's');
+        },
+
+        async addTodo() {
+            var value = this.newTodo && this.newTodo.trim();
+            if (!value) {
+                return;
+            }
+
+            const item = await todoStorage.add({
+                title : value,
+                order: this.todos.length + 1,
+                completed: false
+            });
+            this.todos.push(item);
+            this.newTodo = '';
+        },
+
+        async removeTodo(todo) {
+            await todoStorage.delete(todo);
+            await this.reload();
+        },
+
+        toggleTodo(todo) {
+            todo.completed = ! todo.completed;
+            todoStorage.save(todo);
+        },
+
+        async editTodo(todo) {
+            this.beforeEditCache = todo.title;
+            this.editedTodo = todo;
+        },
+
+        doneEdit(todo) {
+            if (!this.editedTodo) {
+                return;
+            }
+            this.editedTodo = null;
+            todo.title = todo.title.trim();
+            if (!todo.title) {
+                this.removeTodo(todo);
+            } else {
+                todoStorage.save(todo);
+            }
+        },
+
+        cancelEdit(todo) {
+            this.editedTodo = null;
+            todo.title = this.beforeEditCache;
+        },
+
+        async removeCompleted () {
+            await todoStorage.deleteCompleted();
+            await this.reload();
+        },
+
+        async reload() {
+            const data = await todoStorage.fetch();
+            this.todos = data;
+        }
+    },
+
+    async mounted() {
+        await this.reload();
+    },
+
+    directives: {
+        todoFocus
+    }
+});
+
+app.mount('.todoapp');
+
+window.app = app;
