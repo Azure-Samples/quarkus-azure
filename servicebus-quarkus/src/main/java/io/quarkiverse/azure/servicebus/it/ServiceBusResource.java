4fee3ce00e813f755b5ce7a53da363823cd4a05e
Jianguo Ma <jiangma@microsoft.com>
2025-06-04 20:49:02 +0800

    add sample servicebus-quarkus
    Signed-off-by: Jianguo Ma <jiangma@microsoft.com>

---
+++ servicebus-quarkus/src/main/java/io/quarkiverse/azure/servicebus/it/ServiceBusResource.java
@@ -0,0 +1,50 @@
+package io.quarkiverse.azure.servicebus.it;
+
+import java.util.List;
+import java.util.Map;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import org.jboss.logging.Logger;
+
+@Path("/quarkus-azure-servicebus")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ServiceBusResource {
+
+    private static final Logger LOG = Logger.getLogger(ServiceBusResource.class);
+
+    @Inject
+    ServiceBusManager serviceBusManager;
+
+    @POST
+    @Path("/messages")
+    public Response sendMessage(Map<String, String> request) {
+        String message = request.get("message");
+        LOG.infof("REST request to send message: %s", message);
+        serviceBusManager.sendMessage(message);
+
+        return Response.ok(Map.of(
+                "status", "success",
+                "message", "Message sent successfully")).build();
+    }
+
+    @GET
+    @Path("/messages")
+    public Response getMessages() {
+        List<String> messages = serviceBusManager.getReceivedMessages();
+        LOG.infof("REST request to get messages, returning %d messages", messages.size());
+
+        return Response.ok(Map.of(
+                "status", "success",
+                "count", messages.size(),
+                "messages", messages)).build();
+    }
+}
